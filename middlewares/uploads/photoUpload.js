345bd19b93c2847cabd84c815b68a031df807641
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-08 00:47:08 +0600

    Resize photo for post cover

---
+++ middlewares/uploads/photoUpload.js
@@ -0,0 +1,63 @@
+const multer = require("multer")
+const sharp = require("sharp")
+const path = require("path")
+
+
+// Here we are using memorystorage instead of diskstorage since we stored in memory as buffer , it would be helpful for our performance.
+const multerStorage = multer.memoryStorage()
+
+// file type checking
+const multerFilter = (req, file, cb) => {
+    
+    if (file.mimetype.startsWith("image")) {       
+        cb(null, true)
+    } else {
+        cb({message: "Unsupported format"},false)
+    }
+}
+
+
+const photoUpload = multer({
+	storage: multerStorage,
+	fileFilter: multerFilter,
+	// limits: { fileSize: 1000000 }, //file limits to 1 mb
+}).single("image");
+
+
+
+// Resize profile photo
+const profilePhotoResize = async (req, res, next) => {
+    // check there is no file
+    if (!req.file) return next();
+    
+    req.file.filename = `user-${Date.now()}-${req.file.originalname}`;
+
+    
+    await sharp(req.file.buffer)
+      .resize(250, 250)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(path.join(`public/images/profile/${req.file.filename}`))    
+    next()
+
+}
+
+// Resize posts photo
+const postsImgResize = async (req, res, next) => {
+    // check there is no file
+    if (!req.file) return next();
+    
+    req.file.filename = `user-${Date.now()}-${req.file.originalname}`;
+
+    
+    await sharp(req.file.buffer)
+      .resize(500, 500)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(path.join(`public/images/posts/${req.file.filename}`))    
+    next()
+
+}
+
+
+module.exports = { photoUpload, profilePhotoResize, postsImgResize };
