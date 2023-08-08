d9d6de6b7d57dd933ada01dc41df7e50ec9ac547
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-09 02:34:56 +0600

    Created fetch single post , number of post view api

--- route/posts/postRoute.js
+++ route/posts/postRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
 	createPostCtrl,
 	fetchPostsCtrl,
+	fetchSinglePostCtrl,
 } = require("../../controllers/posts/postCtrl");
 const authMiddleware = require("../../middlewares/auth/authMiddleware");
 const {  photoUpload, postsImgResize } = require("../../middlewares/uploads/photoUpload");
@@ -11,9 +12,10 @@ const postRoute = express.Router()
 
 postRoute.post('/', authMiddleware, photoUpload, postsImgResize, createPostCtrl)
 
-
+// Get All Post
 postRoute.get("/", authMiddleware, fetchPostsCtrl); //api/posts
 
-
+// Get single Post
+postRoute.get("/:postId", authMiddleware, fetchSinglePostCtrl);
 
 module.exports = postRoute;
