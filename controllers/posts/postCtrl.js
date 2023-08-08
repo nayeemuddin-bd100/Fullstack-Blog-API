f637afa02692b453c3cf4ac9a76c04508ef79cea
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-09 02:34:37 +0600

    Created fetch single post , number of post view api

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const expressAsyncHandler = require("express-async-handler");
 const Post = require("../../model/post/Post");
+const User = require("../../model/user/User");
 const cloudinaryUpload = require("../../utils/cloudinary");
 const validateMongoDbId = require("../../utils/validateMongoDbId");
 
@@ -16,23 +17,29 @@ const createPostCtrl = expressAsyncHandler(async (req, res) => {
 
 	const { _id } = req.headers.user;
 	validateMongoDbId(_id);
-
+	
 	try {
 		const post = await Post.create({
 			...req.body,
 			author: _id,
 			image: imgUpload,
 		});
-		res.json(post);
 
+		// save post id in user details
+		await User.findByIdAndUpdate(
+			_id,
+			{ $push: { posts: post._id } },
+			{ new: true }
+		);
+
+		res.json(post);
+		
 		// Delete the temporarily saved image file from the server after uploading it to Cloudinary
 		fs.unlinkSync(localPath);
 	} catch (error) {
 		res.json(error);
 	}
 });
-
-
 
 
 /*=============================================
@@ -49,8 +56,32 @@ const fetchPostsCtrl = expressAsyncHandler(async(req, res) => {
 
 })
 
+/*=============================================
+=           Fetch Single Post            =
+=============================================*/
+const fetchSinglePostCtrl = expressAsyncHandler(async (req, res) => {
+	const postId = req.params.postId
+	validateMongoDbId(postId)
+	try {
+		const post = await Post.findById(postId).populate("author");
 
+		// Update number of views
+
+		await Post.findByIdAndUpdate(
+			postId, 
+			{ $inc: { numViews: 1 } },
+			{new:true}
+		)
+		// post.numViews = post.numViews + 1;
+		// await post.save()
+		res.json(post)
+		
+	} catch (error) {
+		res.json(error);
+	}
+})
 module.exports = {
 	createPostCtrl,
 	fetchPostsCtrl,
+	fetchSinglePostCtrl,
 };
