5d5793fea56a3947e9be67c56f6d3b69fa5a6c4d
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-09 02:35:21 +0600

    Rewrite error message

--- utils/validateMongoDbId.js
+++ utils/validateMongoDbId.js
@@ -5,7 +5,7 @@ const validateMongoDbId = (id) => {
     const isValid = mongoose.Types.ObjectId.isValid(id)
 
     if (!isValid) {
-        throw new Error ('User id is not valid or found')
+        throw new Error ('The id is not valids')
     }
 }
 
