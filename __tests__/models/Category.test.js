3dcb34d907851e12743654e10b61dd10f714b5b1
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-17 14:49:06 +0600

    test: add test cases for Comment model

---
+++ __tests__/models/Category.test.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Category = require("../../model/category/Category");
+const User = require("../../model/user/User");
+const {
+	startTestDatabase,
+	stopTestDatabase,
+} = require("../../utils/test-db-setup");
+
+let mongoServer;
+
+beforeAll(async () => {
+	mongoServer = await startTestDatabase();
+});
+
+afterAll(async () => {
+	await stopTestDatabase(mongoServer);
+});
+
+describe("Category Model - unit test", () => {
+	let testUser;
+	beforeEach(async () => {
+		const user = new User({
+			firstName: "Nayeem",
+			lastName: "Uddin",
+			email: "nayeem@gmail.com",
+			password: "password123",
+		});
+
+		testUser = await User.create(user);
+	});
+
+	it("should create new category", async () => {
+		const category = new Category({
+			user: testUser._id,
+			title: "NodeJs",
+		});
+
+		const savedCategory = await category.save();
+
+		expect(savedCategory._id).toBeDefined();
+		expect(savedCategory.user).toBeInstanceOf(mongoose.Types.ObjectId);
+		expect(savedCategory.user).toBe(testUser._id);
+		expect(savedCategory.title).toEqual(expect.any(String));
+	});
+
+	it("should return validation error", async () => {
+		const category = new Category({});
+
+		try {
+			const savedCategory = await category.save();
+		} catch (error) {
+			expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+		}
+	});
+});
