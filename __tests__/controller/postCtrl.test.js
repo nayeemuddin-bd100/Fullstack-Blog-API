d8d98ff07d01773738477ce0b0b0069a7fd4de5d
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-08-29 14:44:12 +0600

    test: check authorized token ✅

--- __tests__/controller/postCtrl.test.js
+++ __tests__/controller/postCtrl.test.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const Category = require("../../model/category/Category");
 const User = require("../../model/user/User");
-// const { connectTestMongoDb, disconnectTestMongoDb } = require("../../utils/test-mongodb-setup");
+const {
+	TokenExpiredError,
+	JsonWebTokenError,
+	NotBeforeError,
+} = require("jsonwebtoken");
 const request = require("supertest");
 const app = require("../../index.js");
 const {
@@ -23,11 +27,26 @@ afterEach(async () => {
 
 describe("Post controller", () => {
 	describe("Get- /api/posts", () => {
+
+		it("should return error if the authorization token is invalid or not provided or expired", async () => {
+			try {
+				const resWithInvalidToken = await request(app)
+					.get("/api/posts")
+					.set("Authorization", "Bearer invalidToken");
+				const resWithNoToken = await request(app)
+					.get("/api/posts")
+				
+				
+			} catch (error) {
+				expect(error).toBeInstanceOf(
+					TokenExpiredError || JsonWebTokenError || NotBeforeError
+				);
+			}
+		});
+
 		it("should return all Post", async () => {
 			// const res = await request(app).get("/api/posts");
-	
 			// expect(res).toBeDefined();
-			
 		});
 	});
 });
