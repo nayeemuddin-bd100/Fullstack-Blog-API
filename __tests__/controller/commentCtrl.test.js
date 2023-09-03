d5a40c67d3792ef3b7b08f5c91838d49985ef7b6
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-09-03 19:11:57 +0600

    test: completed integrated test of comment controller ✅

--- __tests__/controller/commentCtrl.test.js
+++ __tests__/controller/commentCtrl.test.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Comment = require("../../model/comment/Comment");
 const User = require("../../model/user/User");
+const Post = require("../../model/post/Post");
 const request = require("supertest");
 const app = require("../../index");
 const {
@@ -8,9 +9,8 @@ const {
 	stopTestDatabase,
 } = require("../../utils/test-db-setup-ctrl");
 
+
 jest.useRealTimers();
-
-
 
 let mongoServer;
 
@@ -18,11 +18,155 @@ beforeEach(async () => {
 	mongoServer = await startTestDatabase();
 });
 
-afterEach(async () => {
+afterAll(async () => {
 	await stopTestDatabase(mongoServer);
 });
-it('should return error if contain empty comment', async() => {
-   // test code...
-   
-})
+
+describe("Comment Controller", () => {
+	let userData;
+	beforeEach(async () => {
+		//   Register User
+		const user = new User({
+			firstName: "Saiful",
+			lastName: "Islam",
+			email: "saiful@islam.com",
+			password: "TestPass123",
+		});
+		const userResponse = await request(app)
+			.post("/api/users/login")
+			.send({ email: "saiful@islam.com", password: "TestPass123" });
+
+		userData = JSON.parse(userResponse.text);
+	});
+
+
+	describe("POST - /api/comments/", () => {
+		it("should return error if the authorization token is invalid or not provided or expired", async () => {
+			try {
+				const resWithInvalidToken = await request(app)
+					.get("/api/comments")
+					.set("Authorization", "Bearer invalidToken");
+				const resWithNoToken = await request(app).get("/api/comments");
+			} catch (error) {
+				expect(error).toBeInstanceOf(
+					TokenExpiredError || JsonWebTokenError || NotBeforeError
+				);
+			}
+		});
+		it("should create a comment", async () => {
+
+			const post = await Post.findOne({})
+			const postId = post.id;
+
+			const comment = {
+				postId,
+				description: "testing last",
+			};
+
+			const res = await request(app)
+				.post("/api/comments")
+				.send(comment)
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+
+			expect(res.status).toBe(200);
+			expect(res.text).toBeDefined();
+			expect(parsedResponse.description).toBe("testing last");
+		});
+
+		it("should return error if contain empty comment", async () => {
+				const post = await Post.findOne({});
+				const postId = post.id;
+			const comment = {
+				postId,
+				description: "",
+			};
+
+			const res = await request(app)
+				.post("/api/comments")
+				.send(comment)
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+			expect(parsedResponse.name).toBe("ValidationError");
+		});
+	});
+
+	describe("GET - /api/comments/", () => {
+		it("should return all comments", async () => {
+			const res = await request(app)
+				.get("/api/comments")
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+
+			// Assertion
+			expect(res.status).toBe(200);
+			expect(res.text).toBeDefined();
+			expect(parsedResponse).toBeInstanceOf(Array);
+
+		});
+	});
+
+	describe("GET - /api/comments/:commentId", () => {
+		it("should return a specific comments", async () => {
+			
+			const comment = await Comment.findOne({});
+			const commentId = comment._id.toString();
+			const res = await request(app)
+				.get(`/api/comments/${commentId}`)
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+
+			// Assertion
+			expect(res.status).toBe(200);
+			expect(res.text).toBeDefined();
+			expect(parsedResponse._id).toBe(commentId);
+		});
+	});
+
+	describe("PUT - /api/comments/:commentId", () => {
+		it("should update a comment", async () => {
+
+			const comment = await Comment.findOne({});
+			const commentId = comment._id.toString();
+
+			updatedComment = {
+				description: "updating Integrated testing",
+			};
+			const res = await request(app)
+				.put(`/api/comments/${commentId}`)
+				.send(updatedComment)
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+
+			// Assertion
+			expect(res.status).toBe(200);
+			expect(res.text).toBeDefined();
+			expect(parsedResponse._id).toBe(commentId);
+			expect(parsedResponse.description).toBe(updatedComment.description);
+		});
+	});
+	describe("DELETE - /api/comments/:commentId", () => {
+		it("should delete a comment", async () => {
+
+			const comment = await Comment.findOne({});
+			const commentId = comment._id.toString();
+			
+			const res = await request(app)
+				.delete(`/api/comments/${commentId}`)
+				.set("Authorization", `Bearer ${userData.token}`);
+
+			const parsedResponse = JSON.parse(res.text);
+
+			// Assertion
+			expect(res.status).toBe(200);
+			expect(res.text).toBeDefined();
+			expect(parsedResponse._id).toBe(commentId);
+		});
+	});
+});
 
